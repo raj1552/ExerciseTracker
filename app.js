b3bba6453f3d682ffb4e80303796485055ccf52e
Raj Shrestha <anurajshrestha75@gmail.com>
2023-10-17 08:04:36 +0545

    changes on files and api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const port = 3000
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: false }));
@@ -13,13 +14,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.listen(port, ()=> console.log(`app listening on port ${port}!`))
+
 function generateUniqueId() {
 
   return Math.floor(Math.random() * 10000)
 }
 
-const Users = []
-const Exercises =[]
+const Users = [
+  {
+    username: "abcd",
+    _id: 1
+  }
+]
+
+app.get('/api/users', (req, res) => {
+  res.send(Users)
+})
 
 app.post('/api/users', (req, res) => {
 
@@ -31,91 +42,101 @@ app.post('/api/users', (req, res) => {
 
   Users.push(newusers)
 
-  res.json(newusers)
+  res.json({
+    username: username,
+    _id: _id
+  })
 
 })
-
-app.get('/api/users', (req, res) => {
-
-    res.json(Users)
-})
-
 
 app.post('/api/users/:_id/exercises', (req, res) => {
 
   const { _id } = req.params;
-  const { description, duration, date } = req.body;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = new Date(req.body.date).toDateString();
 
-  let user = Users.find((user) => user._id === Number(_id)); 
+  let user = Users.find((user) => user._id === Number(_id));
+
+  if (!description) {
+    return res.json({ description: "Error" })
+  }
+
+  if (!duration) {
+    return res.json({ duration: "Error" })
+  }
 
   if (!user) {
-     return res.json({ error: 'User not found' });
+    return res.json({ error: 'User not found' });
   }
 
-  let exerciseDate;
-
-  if (date === undefined) {
-      exerciseDate= new Date().toDateString();
-  }
-  else{
-      exerciseDate = new Date(date).toDateString()
+  if (new Date(req.body.date).toDateString() === 'Invalid Date') {
+    date = new Date().toDateString();
   }
 
   const exercise = {
-      description: String(description),
-      duration: Number(duration),
-      date: exerciseDate
+    description: String(description),
+    duration: Number(duration),
+    date: date
   };
 
-  Exercises.push(exercise);
+  if (!user.log) {
+    user.log = [];
+  }
 
-  res.json({
-     username : user.username,
-     description : description,
-     duration : duration , 
-     date : exerciseDate,
-     _id : user._id
-  });
+  user.log.push(exercise)
+
+  res.json(user);
 
 });
 
 app.get('/api/users/:_id/exercises', (req, res) => {
 
-  const {_id} = req.params;
-  
+  const { _id } = req.params;
+
   const user = Users.find((user) => user._id === Number(_id))
-  
-  if(!user){
-      return res.json({error: "User not found"})
+
+  if (!user) {
+    return res.json({ error: "User not found" })
   }
 
-  const userExercise = Exercises[user]
-
-  res.json({ username: user.username, log: userExercise });
+  res.json({ username: user.username, log: user.log });
 })
 
-
-app.get('/api/users/:_id/logs' , (req , res) =>{
+app.get('/api/users/:_id/logs', (req, res) => {
 
   const { _id } = req.params;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = parseInt(req.query.limit);
 
   const user = Users.find((user) => user._id === Number(_id));
 
-  if(!user){
-       return res.json({error : "User not found"})
+  if (!user) {
+    return res.json({ error: "User not found" });
+
   }
 
-  const userExercise = Exercises[user]
+  let filteredLogs = user.log;
+
+  if (from && to) {
+    filteredLogs = filteredLogs.filter((log) => {
+      const logDate = new Date(log.date);
+      return logDate >= new Date(from) && logDate <= new Date(to);
+    });
+  }
+
+  if (!isNaN(limit) && limit > 0) {
+    filteredLogs = filteredLogs.slice(0, limit);
+  }
+
   res.json({
-      username : user.username,
-      count : Exercises.length ,
-      _id : user._id,
-      log: userExercise
-  })
-})
+    username: user.username,
+    count: filteredLogs.length,
+    _id: user._id,
+    log: filteredLogs,
+  });
+
+});
 
 
-
-const listener = app.listen(process.env.PORT || 7777, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
